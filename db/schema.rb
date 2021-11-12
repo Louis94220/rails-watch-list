2ef812ff23384872ffb4ea1e90833d53f1cf4ce4
Louis Baconnet <louis.bac@hotmail.fr>
2021-11-12 15:02:35 +0100

    Bundler fix

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,8 @@ ActiveRecord::Schema.define(version: 2021_11_11_113752) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.string "comment"
-    t.bigint "movie_id", null: false
-    t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["list_id"], name: "index_bookmarks_on_list_id"
-    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -40,6 +36,4 @@ ActiveRecord::Schema.define(version: 2021_11_11_113752) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "bookmarks", "lists"
-  add_foreign_key "bookmarks", "movies"
 end
